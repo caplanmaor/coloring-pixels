156f0b719a528cf2aefe1218843ac49fc8a8e0e4
caplanmaor <caplanmaor@gmail.com>
2021-12-22 17:05:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let activeColor = "black";
+let isClicked = false;
+let pixelCount = 1440;
+
+const colorPallete = [
+  "red",
+  "green",
+  "pink",
+  "blue",
+  "yellow",
+  "brown",
+  "teal",
+  "black",
+  "purple",
+  "beige",
+  "Chartreuse",
+  "navy",
+];
+
+createColorPallete();
+createPixelGrid();
+getClearButton();
+
+function createColorPallete() {
+  for (let color of colorPallete) {
+    let colorRect = document.createElement("button");
+    colorRect.classList.add("color-colorRect");
+    colorRect.style.backgroundColor = color;
+    colorRect.addEventListener("click", changeColor);
+    document.getElementById("control-panel").appendChild(colorRect);
+  }
+}
+
+function createPixelGrid() {
+  i = pixelCount;
+  while (i > 0) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.addEventListener("mousedown", setIsClickedOn);
+    pixel.addEventListener("mousedown", fillColor);
+    pixel.addEventListener("mouseup", setIsClickedOff);
+    pixel.addEventListener("mouseover", checkIfClicked);
+    document.getElementById("pixel-grid").appendChild(pixel);
+    i--;
+  }
+}
+
+function changeColor(e) {
+  activeColor = e.target.style.backgroundColor;
+}
+
+function setIsClickedOn() {
+  isClicked = true;
+}
+
+function setIsClickedOff() {
+  isClicked = false;
+}
+
+function checkIfClicked(e) {
+  if (isClicked === true) {
+    fillColor(e);
+  }
+}
+
+function fillColor(e) {
+  e.target.style.backgroundColor = activeColor;
+}
+
+function getClearButton() {
+  clearButton = document.getElementById("clearbtn");
+  clearButton.addEventListener("click", clearScrean);
+}
+
+function clearScrean() {
+  pixels = document.getElementsByClassName("pixel");
+  i = 0;
+  while (i < pixelCount) {
+    pixels[i].style.backgroundColor = "white";
+    i++;
+  }
+}
